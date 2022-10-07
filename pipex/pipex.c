6978a2ec9391c0207cdf3b18305dc783eff4e9a9
Sargis Keveyans <skeveyan@c1r4s5.42yerevan.am>
2022-10-07 19:01:37 +0400

    pipex

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -5,24 +5,89 @@ int main (int argc, char **argv, char **env)
 	int i_fd;
 	int o_fd;
 	char **comands_path;
+	int path_chek_i;
+	int *pip;
+	int pip_i;
+	int comands_pes;
+	pid_t pid;
+	int c_i = 0;
+
 	int n = 0;
 	i_fd =0;
 	o_fd = 0;
-	int pid;
+	pip_i = 0;	
+	path_chek_i = 0;
+	comands_pes = argc - 4;
 
 	if ( argc >=  4)
 	{
 		chekin_file(argv,argc,&i_fd, &o_fd);
-		printf("test\n");
-		comands_path = chekin_comands(argc,argv,env);
-		while( comands_path[n])
+		comands_path = chekin_comands(argc,argv,env,&path_chek_i);
+
+		while ( n < path_chek_i- 1)
 			printf("%s\n",comands_path[n++]);
-//		char **comand;
-		printf("test\n");
+		
+		pip = (int*)malloc(sizeof(int)*comands_pes*2);
+		if(!pip)
+			exit(0);
+		while(pip_i < (comands_pes *2))
+		{
+			pipe(&pip[pip_i*2]);
+			pip_i++;
+		}
+		pip_i = 0;
+//		while(comands_pes-- )
+			//{
+			pid = fork();
+			printf("#%d##\n",pid);
+				if (pid == 0)
+				{
+
+					printf("ferst\n");
+					printf("secend\n");
+					printf("@@@%s@@@\n",comands_path[0]);
+					printf("@@@%s@@@\n",comands_path[1]);
+					dup2(i_fd,0);
+					dup2(pip[++pip_i],1);
+					close(i_fd);
+					close(o_fd);
+					while(pip_i < comands_pes*2)
+						close(pip[pip_i++]);
+
+				//	comans_path[0]= "bin/ls";
+			//		comands_path [3] = NULL;
+				//	comands_path[1] = "-la";
+					execve(comands_path[0],&comands_path[1], env);
+					exit(0);
+				}
+					printf("heto\n");		
+				pid = fork();
+  		if (pid == 0)
+			{
+				printf("secend\n");
+				printf("@@@%s@@@\n",comands_path[4]);
+				printf("@@@%s@@@\n",comands_path[5]);
+				dup2(pip[pip_i],0);
+				dup2(o_fd,1);
+				pip_i = 0;
+				close(i_fd);
+				close(o_fd);
+				while(pip_i < argc*2)
+					close(pip[pip_i++]);
+				execve(comands_path[4],&comands_path[5],env);
+				exit(0);
+			}
+
+				
+			//wait(NULL);
+			wait(NULL);
+//			}
+
+//			char **comand;
 //		comand = (char **)malloc(sizeof(char*) * 3);
 //		if(!comand)
 //			return(0);
-//		comand[0] = ft_strdup("ls");
+//		coma	nd[0] = ft_strdup("ls");
 //		comand[1] = ft_strdup("-ls");
 //		comand[2] = NULL;			
 //		pipe(fdp);
@@ -43,6 +108,7 @@ int main (int argc, char **argv, char **env)
 	}
 	else 
 		write(1,"pipex invalid it`s litel argument",33);
+	//while(1);
 	return(0);
    
 }
@@ -52,7 +118,7 @@ void  chekin_file(char **fp, int argc,int *i_fd,int *o_fd )
 {
 	if((*i_fd = open(fp[1],O_RDWR)) < 0)
 		{
-			perror("sxala");
+			perror("not  input file ");
 			exit(0);
 		}
 	if ((*o_fd = open(fp[argc - 1],O_RDWR | O_CREAT | O_TRUNC ,0644 )) < 0)
@@ -79,55 +145,50 @@ char **faund_path(char **env)
 	return(NULL);
 }
 
-char ** chekin_comands( int argc,char **argv, char **env)
+char ** chekin_comands( int argc,char **argv, char **env,int *path_chek_i)
 {
-	char **comands_path;
-	char **path;
-	char *path_chek;
-	char **comands;
-	int n ;
-	int i;
-	int path_i;
-		
-	path_i = 0; 
-	i = 0;
-	n = 0;
-	path = faund_path(env);
-	comands_path = (char **) malloc(sizeof(char**)*(argc - 2)*3 + 1);
-	if(!comands_path)
+	t_chek_comands var;
+
+	var.path_i = 0;
+	var.comands_n = 0;
+	var.path = faund_path(env);
+	var.comands_path = (char **) malloc(sizeof(char**)*(argc - 3)*4 + 1);
+	if(!var.comands_path)
 		exit(0);
-	while(n < argc - 2)
+	while(var.comands_n < argc - 3)
 	{	
-		comands = ft_split(argv[n + 2],' ');
-		printf("%s\n",comands[0] );
-
-		printf("%s\n",comands[1] );
-		while(path[i] && (access(( path_chek = 
-							ft_strjoin(path[i],ft_strjoin("/",comands[0]))),F_OK)))
+		var.comands = ft_split(argv[var.comands_n + 2],' ');
+	while(var.path[var.path_i] && (access(( var.path_chek = 
+							ft_strjoin(var.path[var.path_i],(var.comand_slesh = ft_strjoin("/",var.comands[0])))),F_OK)))
 				{
-					printf("%s\n",path_chek);
-					free(path_chek);	
-					i++;
+					free(var.path_chek);	
+					free(var.comand_slesh);
+					var.path_i++;
 				}
-		if(path[i] && access(path[i],F_OK ))
+		if(var.path[var.path_i] && !access(var.path_chek,F_OK ))
 			{
-
-				comands_path[path_i++] = path_chek;
-			   	comands_path[path_i++]  = comands[0];
-				comands_path[path_i++] = comands[1];
-			i = 0;	
+				var.comands_path[(*path_chek_i)++] = var.path_chek;
+				free(var.comand_slesh);
+			   	var.comands_path[(*path_chek_i)++] = var.comands[0];
+				var.comands_path[(*path_chek_i)++] = var.comands[1];
+				var.comands_path[(*path_chek_i)++] = NULL;
+				var.path_i = 0;	
 			}
-		else
+    	else
 		{
-			
-		printf("exit" );
-		exit(0);
+			if(var.path_chek)
+				free(var.path_chek);
+			if(var.comand_slesh)
+				free(var.comand_slesh);
+			write(1,"not found comand",20);
+			exit(0);
 		}
-		n++;
+		var.comands_n++;
+		free(var.comands);
 	}
-	comands[path_i++] = NULL;
-	frik(path);
-	return(comands_path);
+		var.comands_path[(*path_chek_i)++] = NULL;
+	frik(var.path);
+	return(var.comands_path);
 }
 
 void frik(char **s1)
--- pipex/pipex.h
+++ pipex/pipex.h
@@ -18,4 +18,16 @@ size_t	need_row(const char *s, char c );
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strdup(const char *s);
 void frik(char **s1);
-char ** chekin_comands( int argc,char **argv, char **env);
+char ** chekin_comands( int argc,char **argv, char **env,int *path_chek_i);
+
+typedef struct T_CHEK_COMANDS{
+	
+	char **comands_path;
+	char **path;
+	char *path_chek;
+	char *comand_slesh;
+	char **comands;
+	int comands_n ;
+	int path_i;	
+}t_chek_comands;
+
